4c18d2644efc6528f7ff4e892beccba53c0b52b6
Brian Hanson <brian@brianhanson.net>
2024-12-09 08:24:19 -0600

    Add licenseKey

--- src/web/assets/ckeditor/src/ckeditor5-craftcms.js
+++ src/web/assets/ckeditor/src/ckeditor5-craftcms.js
@@ -408,6 +408,8 @@ export const create = async function (element, config) {
     element = document.querySelector(`#${element}`);
   }
 
+  config.licenseKey = 'GPL';
+
   const editor = await ClassicEditor.create(
     element,
     Object.assign({plugins}, config),
